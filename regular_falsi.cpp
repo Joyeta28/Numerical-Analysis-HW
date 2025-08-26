fc21bef732d2a64f7bcc33208367d0435f1f6c52
Joyeta Ray <168165453+Joyeta28@users.noreply.github.com>
2025-08-26 23:01:49 +0600

    corrected regular_falsi.cpp

--- regular_falsi.cpp
+++ regular_falsi.cpp
@@ -5,12 +5,11 @@ using namespace std;
 const double TOLERANCE = 1e-4;
 
 double f(double x) {
-    if (x <= 0)
-        return NAN;
+    if (x<=0) return NAN;
     return log(3*x) - 3;
 }
 
-void regulaFalsi(double a, double b) {
+void regulaFalsi(double a,double b) {
     if (f(a) * f(b) >= 0 || isnan(f(a)) || isnan(f(b))) {
         cout << "Invalid interval"<<endl;
         return;
@@ -20,10 +19,11 @@ void regulaFalsi(double a, double b) {
     int iterations = 0;
 
     do {
-        c = (a * f(b) - b * f(a))/(f(b) - f(a));
+
+        c = (a*f(b) - b*f(a))/(f(b) - f(a));
         iterations++;
 
-        if (fabs(f(c)) < TOLERANCE)
+        if (abs(f(c)) < TOLERANCE)
             break;
 
         if (f(c)*f(a) < 0)
@@ -31,20 +31,18 @@ void regulaFalsi(double a, double b) {
         else
             a = c;
 
-    } while (fabs(f(c)) > TOLERANCE);
+    } while(abs(f(c))> TOLERANCE);
 
-    cout << "Regula Falsi Method:\n";
     cout << "Root "<< c <<" found in " <<iterations<<" iterations."<<endl;
 }
 
 int main() {
-    double a, b;
+    double a,b;
     cin>>a>>b;
     if(a<0 || b<0){
         cout<<"invalid input"<<endl;
     }
-    regulaFalsi(a, b);
+    regulaFalsi(a,b);
 
     return 0;
 }
-
